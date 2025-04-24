00b58e550991534a695f6bd339587ba4f8611ab3
Matloc02 <Matthew.rose06@gmail.com>
2025-04-24 15:44:34 -0300

    Fix: Add JSX type import to resolve TS2503 error

--- app/sample-interviews/start/page.tsx
+++ app/sample-interviews/start/page.tsx
@@ -1,13 +1,14 @@
-// app/sample-interviews/start/page.tsx
 import { redirect } from "next/navigation";
 import { sampleInterviews } from "@/lib/interviews/sampleSets";
 import { startSampleInterview } from "@/lib/actions/startSampleInterview";
+import type { JSX } from "react";
+
 
 export default async function SampleInterviewStartPage({
   params,
 }: {
   params: { id: string };
-}) {
+}): Promise<JSX.Element> {
   const interview = sampleInterviews.find((i) => i.id === params.id);
 
   if (!interview) {
